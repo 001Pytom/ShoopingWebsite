8c4c50740740f2620f542acee2a93fcfc19f49c2
faatihat <tomfaah@gmail.com>
2024-05-22 21:13:48 +0100

    adjusted homepageslides

--- src/data/slides.js
+++ src/data/slides.js
@@ -1,7 +1,10 @@
 export const slides = [
+  "../src/assets/pexel2.jpg",
   "../src/assets/homecart.avif",
   "../src/assets/homepage.jpg",
   "../src/assets/homeorgg.webp",
   "../src/assets/homeorg.webp",
   "../src/assets/homeorg2.webp",
+  "../src/assets/pexels1.jpg",
+  "../src/assets/pexels3.jpg",
 ];
