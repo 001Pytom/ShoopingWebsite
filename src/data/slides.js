0ddf5336c4296447d3d432e2fecab98677d045b8
faatihat <tomfaah@gmail.com>
2024-05-22 21:44:06 +0100

    moved assets to public

--- src/data/slides.js
+++ src/data/slides.js
@@ -1,10 +1,10 @@
 export const slides = [
-  "../public/assets/pexel2.jpg",
-  "../public/assets/homecart.avif",
-  "../public/assets/homepage.jpg",
-  "../public/assets/homeorgg.webp",
-  "../public/assets/homeorg.webp",
-  "../public/assets/homeorg2.webp",
-  "../public/assets/pexels1.jpg",
-  "../public/assets/pexels3.jpg",
+  "assets/pexel2.jpg",
+  "assets/homecart.avif",
+  "assets/homepage.jpg",
+  "assets/homeorgg.webp",
+  "assets/homeorg.webp",
+  "assets/homeorg2.webp",
+  "assets/pexels1.jpg",
+  "assets/pexels3.jpg",
 ];
